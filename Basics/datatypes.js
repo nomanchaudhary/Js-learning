9e6ea81ceafbdc15a09908852c93aaa0849523e5
nomanchaudhary <nomanchaudhary1618@gmail.com>
2024-08-15 13:25:53 +0000

    Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,18 @@
+// string Data type
+let Name="Noman"; 
+// Number data type
+let age = 18
+// Truthy and Faslsy
+const isLoggedin=true;
+
+/*
+Data types In Javascript
+string
+number
+boolean
+bigint for big numbers 
+*/
+
+console.log(typeof null); //typeof null is Object
+console.log(typeof undefined); //typeof undefined is undefined 
+
---
+++ Basics/javascriptlec1.js
@@ -0,0 +1,9 @@
+const myName="Noman";
+const myPassword= 123456;
+let myEmail="nomi@google.com"
+/* 
+Only Use Let and const in JS
+Never use var In programming in JS
+*/
+let myShoe=`My Shoe Size Is ${myPassword} `;
+console.table([myName,myEmail,myPassword,myShoe]);
